34504d5872773ef0a65054427ab64f47368391fa
ToastHawaii <mar.pel@protonmail.com>
2020-09-12 22:02:59 +0200

    try fix

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_modules\/(?!idb)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
